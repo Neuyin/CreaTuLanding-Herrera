eea04ee0c23b196dd83f9fc92cdff0d21b13c08d
Neuyin Herrera <neuyin.herrera.sun@gmail.com>
2024-03-07 19:55:25 -0300

    Info de contacto

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -2,7 +2,28 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Información de contacto</div>
+    <div>Información de contacto
+      <form class="form-contacto" action="https://formspree.io/f/xoqoklrz" method="POST">
+                <div class="mb-3">
+                    <label for="exampleInputName1" class="form-label">Nombre y apellido</label>
+                    <input type="text" class="form-control" id="exampleInputName1" placeholder="Escribe aquí tu nombre y apellido" name="nombre"/>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Email</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="name@example.com" name="email"/>
+                    <div id="emailHelp" class="form-text">Nunca enviaré tu correo a nadie más.</div>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Teléfono</label>
+                    <input type="number" class="form-control" id="exampleInputPassword1" placeholder="Escribe tu número de contacto" name="telefono"/>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlTextarea1" class="form-label">Mensaje</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="mensaje" placeholder="Escribe aquí tus comentarios o motivo de consulta"></textarea>
+                </div> 
+                <button type="submit" class="btn btn-contacto">Enviar</button>
+            </form>   
+    </div>
   )
 }
 
